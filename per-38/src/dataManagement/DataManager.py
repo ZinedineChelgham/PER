f4b75ea9089414a11a33afa4b08919a1f13faeb0
Aymaneelfiguigui <90783893+Aymaneelfiguigui@users.noreply.github.com>
2023-12-21 04:35:33 +0100

    transforming data to more structred one

---
+++ per-38/src/dataManagement/DataManager.py
@@ -0,0 +1,29 @@
+import json
+
+# Load your JSON data here. For this example, it's loaded from a file.
+with open('done.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+# Skip the first chunk as it's general info
+question_chunk = data[1]  # This is the second chunk with questions
+respondents_data = data[2:]  # All other chunks are respondents' answers
+
+# Create a dictionary to map 'ColumnX' to the actual question
+questions = {key: question_chunk[key] for key in question_chunk}
+
+# Function to transform a respondent's answers
+def transform_answers(answers):
+    return {questions.get(key, key): answers.get(key, None) for key in questions}
+
+# Transform each respondent's data
+transformed_data = [transform_answers(resp) for resp in respondents_data]
+
+# Convert the transformed data back to JSON (or use it as a Python dict)
+transformed_json = json.dumps(transformed_data, indent=4)
+
+#write the data to a json file
+with open('transformed.json', 'w') as file:
+    file.write(transformed_json)
+
+# For demonstration, printing the first few entries
+# print(transformed_json[:1000])  # Print a portion to check the structure
