c6db8a5884f3667939ad2e041d5f45d8b339d5a1
ZC <zinedine.chelgham@etu.univ-cotedazur.fr>
2024-02-01 20:10:05 +0100

    Add responsive layout and carousel to Presentation component

--- per-38/src/pagesComponents/Presentation.js
+++ per-38/src/pagesComponents/Presentation.js
@@ -2,41 +2,62 @@ import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import React from "react";
 import Typography from "@mui/material/Typography";
 import map from "../assets/images/map.png";
+import { carousselItems } from "../utils/carousselItemExporter";
+import Carousel from "react-material-ui-carousel";
 
 function Presentation() {
   return (
-    <Grid2 container direction={"row"} height={"100%"} spacing={12}>
+    <Grid2
+      container
+      direction={"row"}
+      height={"100%"}
+      spacing={12}
+      paddingTop={6}
+    >
       <Grid2 xs={6} height={"100%"}>
-        <Grid2 direction={"column"}>
-          <Typography variant="h6" marginBottom={1}>
+        <Grid2 direction={"column"} paddingLeft={4}>
+          <Typography variant="h5" marginBottom={1}>
             Qu'est-ce que c'est ?
           </Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          eveniet exercitationem pariatur. Animi dolorum harum reprehenderit?
-          Accusamus amet libero tempora accusantium corporis, deserunt,
-          molestiae quo soluta id aut, maxime velit.
-          <Typography variant="h6" marginBottom={1}>
+          <Typography marginBottom={2}>
+            Le CA de Spécif Campus a, courant 2023, élaboré un questionnaire
+            portant sur l’évolution du métier d’enseignant-chercheur basé sur
+            cinq axes : généralités, enseignement, recherche, responsabilités
+            administratives et carrière.
+          </Typography>
+          <Typography variant="h5" marginBottom={1}>
             Pourquoi ?
           </Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          eveniet exercitationem pariatur. Animi dolorum harum reprehenderit?
-          Accusamus amet libero tempora accusantium corporis, deserunt,
-          molestiae quo soluta id aut, maxime velit.
-          <Typography variant="h6" marginBottom={1}>
+          <Typography marginBottom={2}>
+            Il a été soumis à tous les laboratoires d’informatique dans le but
+            de mesurer l'impact que peut avoir l'évolution du métier sur le
+            quotidien des interrogés.
+          </Typography>
+          <Typography variant="h5" marginBottom={1}>
             D'où proviennent ces données ?
           </Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          eveniet exercitationem pariatur. Animi dolorum harum reprehenderit?
-          Accusamus amet libero tempora accusantium corporis, deserunt,
-          molestiae quo soluta id aut, maxime velit.
-          <Typography variant="h6" marginBottom={1}>
+          <Typography marginBottom={2}>
+            Les données présentées ici sont issues du traitement des réponses au
+            questionnaire.
+          </Typography>
+          <Typography variant="h5" marginBottom={1}>
             Votre avis compte
+          </Typography>
+          <Typography>
+            Vous pouvez contribuer à l'amélioration de ce site en faisant part
+            de vos remarques à l'adresse suivante : <a href="mailto:xx">xx</a>
           </Typography>
         </Grid2>
       </Grid2>
 
-      <Grid2 xs={6} height={"100%"}>
-        <img src={map} alt="" />
+      <Grid2 xs={6} height={"100%"} width={"fit-parent"}>
+        <Carousel>
+          {carousselItems.map((item) => (
+            <div key={item.id}>
+              <img src={item.image} alt={item.title} />
+            </div>
+          ))}
+        </Carousel>
       </Grid2>
     </Grid2>
   );
