b1ef5480f48223ffc1499bb5e9a0017e35fa47b3
SAISSIOMAR <omar.saissi@etu.univ-cotedazur.fr>
2024-02-01 17:24:36 +0100

    first "mono critère" visu

--- per-38/src/pagesComponents/Cadre.js
+++ per-38/src/pagesComponents/Cadre.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 import { Tabs, Tab, Typography, Box } from "@mui/material";
 import TabPanel from "../utilsComponents/TabPanel";
+import PieChart from "../graphics/PieChart";
 
+const data = [
+    { category: 'Pas du tout', percentage: 5.59 },
+    { category: 'Plutôt pas', percentage: 7.49 },
+    { category: 'Moyennement', percentage: 18.12 },
+    { category: 'Un peu', percentage: 35.01 },
+    { category: 'Tout à fait', percentage: 33.24 }
+];
+const title = "Métier toujours intéressant ?";
 function Cadre() {
   const [value, setValue] = useState(0);
 
@@ -24,7 +33,7 @@ function Cadre() {
         />
       </Tabs>
       <TabPanel value={value} index={0}>
-        <Typography>Contenu de l'onglet Monocritère</Typography>
+        <Typography><PieChart data={data} title={title} /></Typography>
       </TabPanel>
       <TabPanel value={value} index={1}>
         <Typography>Contenu de l'onglet Multicritère</Typography>
